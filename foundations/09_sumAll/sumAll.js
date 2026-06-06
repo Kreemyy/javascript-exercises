a7d1dfff8dceb0507b89a0aa96fc9ebe242e8753
Kareem <abdulkareemogedemgbe@outlook.com>
2026-06-06 18:06:26 +0100

    Create a function that sums all values between two positive integers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || Math.floor(a) !== a) return "ERROR";
+  if (b < 0 || Math.floor(b) !== b) return "ERROR";
 
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else if (a > b) {
+    for (let i = a; i >= b; i--) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
