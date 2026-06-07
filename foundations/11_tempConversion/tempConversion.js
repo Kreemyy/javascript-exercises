2177e781b938719dffd76f4cf5dd04ee248d460d
Kareem <abdulkareemogedemgbe@outlook.com>
2026-06-07 18:14:12 +0100

    Make a function that converts temperature between °C and F

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+  const celcius = (F - 32) * (5 / 9);
+  return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C) {
+  const fahrenheit = (C * 9) / 5 + 32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
