1afde778cbc67b61d39556f0da6f0448689bbf4e
Kareem <abdulkareemogedemgbe@outlook.com>
2026-07-07 22:49:54 +0100

    Create various calculator functions
    Create functions to add, subtract, multiply, find power and factorials

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, num) => {
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, num) => {
+    return total * num;
+  });
 };
 
-const power = function() {
-	
+const power = function (base, expo) {
+  return base ** expo;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorial = 1;
+  for (let i = num; i > 0; i--) {
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
