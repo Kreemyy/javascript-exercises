c662063c674aea5ab4c4a9581269477c4576fdb0
Kareem <abdulkareemogedemgbe@outlook.com>
2026-07-08 13:34:27 +0100

    Write a function to determine a string palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let cleanedStr = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => {
+      return /[^\s\p{P}\p{S}]/gu.test(char);
+    })
+    .join("");
 
+  let reversedString = "";
+
+  for (let i = cleanedStr.length - 1; i >= 0; i--) {
+    reversedString += cleanedStr.charAt(i);
+  }
+
+  return reversedString === cleanedStr;
 };
 
 // Do not edit below this line
