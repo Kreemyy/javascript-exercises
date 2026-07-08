0ecbaf25b0f30d143d46e6cc909001b8b56b51c1
Kareem <abdulkareemogedemgbe@outlook.com>
2026-07-08 15:06:18 +0100

    Write a function that finds a member of a fibonacci
    Create a function to return a specific member of a fibonacci sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+  if (typeof member === "string") {
+    member = +member;
+  }
 
+  if (member < 0) {
+    return "OOPS";
+  } else if (member === 0) {
+    return member;
+  }
+
+  let arr = [1, 1, 2, 3, 5, 8];
+
+  for (let i = 6; i <= member - 1; i++) {
+    let nextNum = arr[i - 1] + arr[i - 2];
+    arr.push(nextNum);
+  }
+
+  return arr[member - 1];
+
+  //   return arr.find((item, index) => {
+  //     if (member === index + 1) {
+  //       return item;
+  //     }
+  //   });
 };
 
 // Do not edit below this line
