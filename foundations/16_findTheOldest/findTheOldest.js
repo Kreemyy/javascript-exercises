d8b6321670facbb29034fd89a16a10af2905e7af
Kareem <abdulkareemogedemgbe@outlook.com>
2026-07-08 17:38:07 +0100

    Write a function to find the oldest person in an array of objects

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.sort((lastPerson, nextPerson) => {
+    let aDeathDay = lastPerson.yearOfDeath || new Date().getFullYear();
+    let bDeathDay = nextPerson.yearOfDeath || new Date().getFullYear();
 
+    ageA = aDeathDay - lastPerson.yearOfBirth;
+    ageB = bDeathDay - nextPerson.yearOfBirth;
+
+    return ageB - ageA;
+  });
+
+  return oldest[0];
 };
 
 // Do not edit below this line
